bca0e2399578998af88674813433ff54e4c7109d
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-09-17 17:40:33 +0900

    [level 2] Title: 더 맵게, Time: 674.83 ms, Memory: 140 MB -BaekjoonHub

--- Java/프로그래머스/(lv.2) [42626] 더 맵게/더 맵게.java
+++ Java/프로그래머스/(lv.2) [42626] 더 맵게/더 맵게.java
@@ -1,23 +1,33 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        int answer = 0;
-
-        for(int s : scoville){
-            minHeap.offer(s);
+        int count = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        
+        for (int s : scoville){
+            pq.add(s);
         }
         
-        while(minHeap.size()>=2 && minHeap.peek() < K){
-            int x = minHeap.poll();
-            int y = minHeap.poll();
-            minHeap.offer(x + y*2);
-            answer++;
-        }
-        
-        if (minHeap.size()==1 && minHeap.peek() < K) return -1;
-        
-        return answer;
+        while (pq.peek() < K) {
+            if (pq.size() < 2) return -1;
+            
+            int a = pq.poll();
+            int b = pq.poll();
+            int scov = a+b*2;
+            
+            pq.add(scov);
+            
+            count ++;
+            
+        } 
+        return count;
     }
 }
+
+/*
+문제 해결
+- 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+
+
+*/
